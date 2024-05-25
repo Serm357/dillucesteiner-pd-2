40741b414da181bb405f219ece2b10b4ecb78cad
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-05-25 15:28:10 +0000

    img

--- app/components/HomePageComponents/Hero.tsx
+++ app/components/HomePageComponents/Hero.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="w-full h-screen bg-[url('/vid2.gif')] bg-no-repeat bg-cover bg-center p-4 text-center">
-      <div className="flex flex-col md:flex-row-reverse">
-        <div className="flex-1 mt-3 bg-primary-foreground rounded-md flex flex-col justify-center items-center">
+    <section className="w-full h-[78vh] overflow-hidden bg-[url('/vid2.gif')] bg-no-repeat bg-cover bg-center p-4 text-center">
+      <div className="flex flex-col gap-2 md:">
+        <div className="flex-1 py-2 pb-2 mt-3 bg-primary-foreground rounded-md flex flex-col justify-center items-center">
           <h1 className="mt-5 text-center font-bold text-xl text-slate-700 dark:text-slate-200">
             Creating a world where <br className="md:idden" />{" "}
             <span className="text-5xl md:text-4xl lg:text-5xl text-primary">
@@ -18,12 +18,13 @@ export default function Hero() {
             architecto molestiae a facilis exercitationem.
           </p>
         </div>
-        <div className="">
+        <div className="w-[200px] h-[200px] md:w-[250px] md:h-[250px] mt-3 flex flex-col items-center justify-center">
           <Image
             src="/female_cutout.webp"
             alt="female cutout"
-            fill
-
+            width={150}
+            height={150}
+            className="w-full"
             //  className="absolute left-0 bottom-0"
           />
         </div>
