0cda9f0d298e36c56dfbf298a2ece92487b18c91
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-05-25 09:51:22 +0000

    hero vid

--- app/components/HomePageComponents/Hero.tsx
+++ app/components/HomePageComponents/Hero.tsx
@@ -1,3 +1,22 @@
-export default function Hero(){
-    return <section>Hero</section>
+export default function Hero() {
+  return (
+    <section className="w-full h-screen bg-[url('/vid2.gif')] bg-no-repeat bg-cover bg-center p-4 text-center">
+      <div className="flex flex-col md:flex-row-reverse">
+        <div className="flex-1 bg-primary-foreground rounded-md flex flex-col justify-center items-center">
+          <h1 className="mt-5 text-center font-bold text-xl text-slate-700 dark:text-slate-200">
+            Creating a world where <br className="md:idden" />{" "}
+            <span className="text-5xl md:text-4xl lg:text-5xl text-primary">
+              Cancer Can’t Hide.
+            </span>
+          </h1>
+          <p className="text-md">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            incidunt necessitatibus, doloremque ex veritatis maiores, cum dolor
+            distinctio dignissimos illum nostrum minima deleniti sed quod
+            architecto molestiae a facilis exercitationem.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
