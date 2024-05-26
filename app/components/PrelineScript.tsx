4b5590453f449540408550e67bc0d5bce611ddb0
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-05-26 23:11:19 +0300

    fix dep

--- app/components/PrelineScript.tsx
+++ app/components/PrelineScript.tsx
@@ -3,25 +3,29 @@
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-import { IStaticMethods } from "preline/preline";
-declare global {
-  interface Window {
-    HSStaticMethods: IStaticMethods;
-  }
-}
+const isBrowser = typeof window !== undefined; // check if component is rendered in a browser
 
-export default function PrelineScript() {
+export default function PrelineLoader() {
   const path = usePathname();
 
   useEffect(() => {
-    const loadPreline = async () => {
-      await import("preline/preline");
+    if (isBrowser) {
+      // if this component is rendered on a browser, import preline
+      import("preline/preline");
+    }
+  }, []);
 
-      window.HSStaticMethods.autoInit();
-    };
-
-    loadPreline();
+  useEffect(() => {
+    setTimeout(() => {
+      if (isBrowser) {
+        // if this component is rendered on a browser, import relevant preline plugins
+        import("preline/preline").then(({ HSAccordion, HSDropdown }) => {
+          HSAccordion.autoInit();
+          HSDropdown.autoInit();
+        });
+      }
+    }, 100);
   }, [path]);
 
-  return null;
+  return <></>;
 }
