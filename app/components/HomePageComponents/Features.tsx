f5851755f991249bbbc8617a62acb7c961b63324
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-05-29 12:27:20 +0300

    features

--- app/components/HomePageComponents/Features.tsx
+++ app/components/HomePageComponents/Features.tsx
@@ -6,13 +6,13 @@ export default function Herofeatures() {
     <section>
       <Heading />
       {/* <!-- Features --> */}
-      <div className=" w-full px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div className="relative p-6 md:p-16">
           {/* <!-- Grid --> */}
           <div className="relative z-10 lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
             <div className="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-8 lg:order-2">
               <h2 className="text-2xl text-gray-800 font-bold sm:text-3xl dark:text-neutral-200">
-                Fully customizable rules to match your unique needs
+                Lorem ipsum dolor sit amet.
               </h2>
 
               {/* <!-- Tab Navs --> */}
@@ -23,7 +23,7 @@ export default function Herofeatures() {
               >
                 <button
                   type="button"
-                  className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-neutral-700 dark:hover:bg-neutral-700"
+                  className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-neutral-700 dark:hover:bg-neutral-700 active"
                   id="tabs-with-card-item-1"
                   data-hs-tab="#tabs-with-card-1"
                   aria-controls="tabs-with-card-1"
@@ -33,12 +33,11 @@ export default function Herofeatures() {
                     <Stars />
                     <span className="grow ms-6">
                       <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-neutral-200">
-                        Detection
+                        Density
                       </span>
-
                       <span className="block mt-1 text-gray-800 dark:hs-tab-active:text-gray-200 dark:text-neutral-200">
-                        Find cancers earlier, with greater accuracy and
-                        efficiency. With up to 2x enhanced clinical performance
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Cum consectetur in ex illo dolorum sit!
                       </span>
                     </span>
                   </span>
@@ -56,13 +55,12 @@ export default function Herofeatures() {
                     <Stars />
                     <span className="grow ms-6">
                       <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-neutral-200">
-                        Density
+                        Smart screening
                       </span>
-
                       <span className="block mt-1 text-gray-800 dark:hs-tab-active:text-gray-200 dark:text-neutral-200">
-                        Breast density can elevate breast cancer risk up to 4x,6
-                        therefore, density scores impact and inform personalized
-                        treatment plans.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reprehenderit placeat numquam, est tenetur molestiae
+                        nihil.
                       </span>
                     </span>
                   </span>
@@ -77,16 +75,31 @@ export default function Herofeatures() {
                   role="tab"
                 >
                   <span className="flex">
-                    <Stars />
+                    <svg
+                      className="flex-shrink-0 mt-2 size-6 md:size-7 hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-neutral-200"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
+                      <path d="M5 3v4" />
+                      <path d="M19 17v4" />
+                      <path d="M3 5h4" />
+                      <path d="M17 19h4" />
+                    </svg>
                     <span className="grow ms-6">
                       <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-neutral-200">
-                        Risk
+                        Detection
                       </span>
-
                       <span className="block mt-1 text-gray-800 dark:hs-tab-active:text-gray-200 dark:text-neutral-200">
-                        The world’s first image-based risk model is 2.4x more
-                        accurate than traditional models.1,2 Identify women at
-                        high risk of developing cancer
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vel deleniti esse molestias dolorem assumenda itaque.
                       </span>
                     </span>
                   </span>
@@ -105,12 +118,10 @@ export default function Herofeatures() {
                     role="tabpanel"
                     aria-labelledby="tabs-with-card-item-1"
                   >
-                    <Image
-                      className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/20 w-full"
+                    <img
+                      className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/20"
                       src="https://images.unsplash.com/photo-1605629921711-2f6b00c6bbf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
                       alt="Image Description"
-                      width={987}
-                      height={1220}
                     />
                   </div>
 
@@ -120,12 +131,10 @@ export default function Herofeatures() {
                     role="tabpanel"
                     aria-labelledby="tabs-with-card-item-2"
                   >
-                    <Image
-                      className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/20 w-full"
+                    <img
+                      className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/20"
                       src="https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
                       alt="Image Description"
-                      width={987}
-                      height={1220}
                     />
                   </div>
 
@@ -135,12 +144,10 @@ export default function Herofeatures() {
                     role="tabpanel"
                     aria-labelledby="tabs-with-card-item-3"
                   >
-                    <Image
-                      className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/20 w-full"
+                    <img
+                      className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/20"
                       src="https://images.unsplash.com/photo-1598929213452-52d72f63e307?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
                       alt="Image Description"
-                      width={987}
-                      height={1220}
                     />
                   </div>
                 </div>
@@ -159,20 +166,20 @@ export default function Herofeatures() {
                     <path
                       d="M5 16.4754C11.7688 27.4499 21.2452 57.3224 5 89.0164"
                       stroke="currentColor"
-                      strokeWidth="10"
-                      strokeLinecap="round"
+                      stroke-width="10"
+                      stroke-linecap="round"
                     />
                     <path
                       d="M33.6761 112.104C44.6984 98.1239 74.2618 57.6776 83.4821 5"
                       stroke="currentColor"
-                      strokeWidth="10"
-                      strokeLinecap="round"
+                      stroke-width="10"
+                      stroke-linecap="round"
                     />
                     <path
                       d="M50.5525 130C68.2064 127.495 110.731 117.541 116 78.0874"
                       stroke="currentColor"
-                      strokeWidth="10"
-                      strokeLinecap="round"
+                      stroke-width="10"
+                      stroke-linecap="round"
                     />
                   </svg>
                 </div>
@@ -185,7 +192,7 @@ export default function Herofeatures() {
 
           {/* <!-- Background Color --> */}
           <div className="absolute inset-0 grid grid-cols-12 size-full">
-            <div className="col-span-full lg:col-span-7 lg:col-start-6 bg-gray-100 w-full h-5/6 rounded-xl sm:h-3/4 lg:h-full dark:bg-neutral-800  lg:hidden"></div>
+            <div className="col-span-full lg:col-span-7 lg:col-start-6 bg-gray-100 w-full h-5/6 rounded-xl sm:h-3/4 lg:h-full dark:bg-neutral-800"></div>
           </div>
           {/* <!-- End Background Color --> */}
         </div>
