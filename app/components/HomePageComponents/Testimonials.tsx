106b11bb786869b06ed0121fed0176ef01df1e51
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-05-29 18:15:34 +0300

    fix height

--- app/components/HomePageComponents/Testimonials.tsx
+++ app/components/HomePageComponents/Testimonials.tsx
@@ -205,14 +205,14 @@ function Testimonial({
               </div>
             </footer>
 
-            <div className="mt-8 lg:mt-14">
+            {/* <div className="mt-8 lg:mt-14">
               <a
                 className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none dark:bg-white dark:text-neutral-800"
                 href="#"
               >
                 Read the story
               </a>
-            </div>
+            </div> */}
           </blockquote>
           {/* <!-- End Blockquote --> */}
         </div>
