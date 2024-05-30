daaa3cdf42998b4e846f9db85a6cb57d659e9af1
topgunserm <topgunserm@gmail.com>
2024-05-30 15:45:29 +0000

    fix bug

--- app/components/HomePageComponents/Stats.tsx
+++ app/components/HomePageComponents/Stats.tsx
@@ -1,6 +1,12 @@
-"use client";
 
-import AnimatedNumbers from "react-animated-numbers";
+import dynamic from "next/dynamic";
+const AnimatedNumbers = dynamic(
+  () => import("react-animated-numbers"),
+  {
+    ssr: false,
+  }
+);
+
 
 export default function Stats() {
   return (
@@ -12,23 +18,41 @@ export default function Stats() {
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         {/* <!-- Grid --> */}
         <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
-          <Card title="Sepsis" no={13660000} desc={'millionpeople who died globally had sepsis as an immediate cause of death or in the chain of events leading to their death (intermediate cause).'} year="2019" />
-          <Card title="Drug Resistant Infection" no={4950000} desc={'millionpeople who died from drug-resistant infections, such as lower respiratory, bloodstream, and intra-abdominal infections'}  year="2019"/>
-          <Card title="Direct Cause" no={1270000} desc={'milliondeaths in 2019 were directly caused by AMR'}  year="2019"/>
-<Card2/ >
-          
+          <Card
+            title="Sepsis"
+            no={13660000}
+            desc={
+              "millionpeople who died globally had sepsis as an immediate cause of death or in the chain of events leading to their death (intermediate cause)."
+            }
+            year="2019"
+          />
+          <Card
+            title="Drug Resistant Infection"
+            no={4950000}
+            desc={
+              "millionpeople who died from drug-resistant infections, such as lower respiratory, bloodstream, and intra-abdominal infections"
+            }
+            year="2019"
+          />
+          <Card
+            title="Direct Cause"
+            no={1270000}
+            desc={"milliondeaths in 2019 were directly caused by AMR"}
+            year="2019"
+          />
+          <Card2 />
         </div>
         {/* <!-- End Grid --> */}
       </div>
     </section>
-  ); 
-} 
+  );
+}
 
 function Card({
   title,
   no,
   desc,
-  year
+  year,
 }: {
   title: string;
   no: number;
@@ -37,74 +61,67 @@ function Card({
 }) {
   return (
     <div className="">
-    <div className=" w-full h-[300px] relative max-w-xs">
-      <div className="absolute inset-0 h-full w-full  transform scale-[0.80] rounded-full blur-3xl" />
-      <div className="relative shadow-xl bg-primary-foreground  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-        
+      <div className=" w-full h-[300px] relative max-w-xs">
+        <div className="absolute inset-0 h-full w-full  transform scale-[0.80] rounded-full blur-3xl" />
+        <div className="relative shadow-xl bg-primary-foreground  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="font-bold text-md text-primary mb-4 relative z-50 uppercase">
+              {title}
+            </h1>
+            <h1 className="font-bold text-2xl text-green-300 mb-4 relative z-50">
+              <AnimatedNumbers
+                includeComma
+                className="text-center"
+                transitions={(index) => ({
+                  type: "spring",
+                  duration: index + 0.3,
+                })}
+                animateToNumber={no}
+                // fontStyle={{
+                //   fontSize: 40,
+                //   color: "red",
+                // }}
+              />
+            </h1>
 
-       <div className="flex flex-col items-center justify-center"> 
-       <h1 className="font-bold text-md text-primary mb-4 relative z-50 uppercase">
-          {title}
-        </h1>
-        <h1 className="font-bold text-2xl text-green-300 mb-4 relative z-50">
-        <AnimatedNumbers
-            includeComma
-            className="text-center"
-            transitions={(index) => ({
-              type: "spring",
-              duration: index + 0.3,
-            })}
-            animateToNumber={no}
-            // fontStyle={{
-            //   fontSize: 40,
-            //   color: "red",
-            // }}
-          />
-        </h1>
+            <p className="font-normal text-base text-slate-500 mb-4 relative z-50 capitalize">
+              {desc}
+            </p>
+            <h1 className="font-bold text-xl  mb-4 relative z-50">{year}</h1>
+          </div>
 
-        <p className="font-normal text-base text-slate-500 mb-4 relative z-50 capitalize">
-          {desc}
-        </p>
-        <h1 className="font-bold text-xl  mb-4 relative z-50">
-          {year}
-        </h1></div>
-     
-
-        {/* Meaty part - Meteor effect */}
-        {/* <Meteors number={70} /> */}
+          {/* Meaty part - Meteor effect */}
+          {/* <Meteors number={70} /> */}
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
-function Card2(){
+function Card2() {
   return (
     <div className="">
-    <div className=" w-full h-[300px] relative max-w-xs">
-      <div className="absolute inset-0 h-full w-full  transform scale-[0.80] rounded-full blur-3xl" />
-      <div className="relative shadow-xl bg-primary-foreground  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-        
+      <div className=" w-full h-[300px] relative max-w-xs">
+        <div className="absolute inset-0 h-full w-full  transform scale-[0.80] rounded-full blur-3xl" />
+        <div className="relative shadow-xl bg-primary-foreground  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="font-bold text-md text-primary mb-4 relative z-50 uppercase">
+              AMR
+            </h1>
+            <h1 className="font-bold text-2xl text-green-300 mb-4 relative z-50">
+              1 in 5 <br /> <span className="text-md ">people</span>
+            </h1>
 
-       <div className="flex flex-col items-center justify-center"> 
-       <h1 className="font-bold text-md text-primary mb-4 relative z-50 uppercase">
-          AMR
-        </h1>
-        <h1 className="font-bold text-2xl text-green-300 mb-4 relative z-50">
-        1 in 5 <br/>   <span className="text-md ">people</span>
-        </h1>
-
-        <p className="font-normal text-base text-slate-500 mb-4 relative z-50 capitalize">
-         died from AMR was a child under 5 years old, often from previously treatable infections.
-        </p>
-        {/* <h1 className="font-bold text-xl text-white mb-4 relative z-50">
+            <p className="font-normal text-base text-slate-500 mb-4 relative z-50 capitalize">
+              died from AMR was a child under 5 years old, often from previously
+              treatable infections.
+            </p>
+            {/* <h1 className="font-bold text-xl text-white mb-4 relative z-50">
           {year}
         </h1> */}
+          </div>
         </div>
-     
-
       </div>
     </div>
-  </div>
-  )
+  );
 }
