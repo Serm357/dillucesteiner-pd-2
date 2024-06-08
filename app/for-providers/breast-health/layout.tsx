16cdf88f3ad7245c3832ff1dba6e7c3b4ec93a49
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-06-08 13:19:06 +0300

    all routes

--- app/for-providers/breast-health/layout.tsx
+++ app/for-providers/breast-health/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Privacy",
+  title: "Breast Health",
   description: "Susceptibility Testing meets AI",
 };
 
--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,47 +1,55 @@
-export default function PrivacyPolicyPage() {
-  <div className="min-h-screen mx-auto">
-    <h1 className="text-primary text-2xl my-6 lg:text-4xl">Terms & Privacy</h1>
-    <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
-      ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
-      autem praesentium quas, ab beatae, quaerat magnam voluptate veniam! Magni,
-      provident?
-    </p>
-    <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
-      ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
-      autem praesentium quas, ab beatae, quaerat magnam voluptate veniam! Magni,
-      provident?
-    </p>
-    <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
-      ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
-      autem praesentium quas, ab beatae, quaerat magnam voluptate veniam! Magni,
-      provident?
-    </p>
-    <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
-      ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
-      autem praesentium quas, ab beatae, quaerat magnam voluptate veniam! Magni,
-      provident?
-    </p>
-    <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
-      ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
-      autem praesentium quas, ab beatae, quaerat magnam voluptate veniam! Magni,
-      provident?
-    </p>
-    <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
-      ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
-      autem praesentium quas, ab beatae, quaerat magnam voluptate veniam! Magni,
-      provident?
-    </p>
-  </div>;
-}
+import React from "react";
+
+const PrivacyPolicyPage = () => {
+  return (
+    <div className="min-h-screen mx-4 p-2">
+      <h1 className="text-primary text-2xl my-6 lg:text-4xl">
+        Terms & Privacy
+      </h1>
+      <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
+        ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
+        autem praesentium quas, ab beatae, quaerat magnam voluptate veniam!
+        Magni, provident?
+      </p>
+      <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
+        ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
+        autem praesentium quas, ab beatae, quaerat magnam voluptate veniam!
+        Magni, provident?
+      </p>
+      <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
+        ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
+        autem praesentium quas, ab beatae, quaerat magnam voluptate veniam!
+        Magni, provident?
+      </p>
+      <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
+        ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
+        autem praesentium quas, ab beatae, quaerat magnam voluptate veniam!
+        Magni, provident?
+      </p>
+      <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
+        ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
+        autem praesentium quas, ab beatae, quaerat magnam voluptate veniam!
+        Magni, provident?
+      </p>
+      <h2 className="my-2 text-green-400">Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit
+        ratione numquam architecto. Ea fuga recusandae facere dolorum officiis
+        autem praesentium quas, ab beatae, quaerat magnam voluptate veniam!
+        Magni, provident?
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicyPage;
