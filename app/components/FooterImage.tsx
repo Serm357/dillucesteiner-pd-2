28a61a8fc4023a1ac609788639cb1b083eeaf628
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-06-16 13:54:42 +0300

    ml

--- app/components/FooterImage.tsx
+++ app/components/FooterImage.tsx
@@ -7,10 +7,10 @@ const FooterImage = () => {
   return (
     <>
       {theme == "dark" && (
-        <img src="/logos/steiner-2.png" className="size-20  ml-5" />
+        <img src="/logos/steiner-2.png" className="size-20  ml-1" />
       )}
       {theme == "light" && (
-        <img src="/logos/steiner-1.png" className="size-20  ml-5" />
+        <img src="/logos/steiner-1.png" className="size-20  ml-1" />
       )}
     </>
   );
