9f990c283a988f3ed47989f756f47606477d3817
Salum Kilangalila <136128813+Serm357@users.noreply.github.com>
2024-07-03 17:36:18 +0300

    fixed system theme

--- app/components/FooterImage.tsx
+++ app/components/FooterImage.tsx
@@ -4,13 +4,25 @@ import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
 
 const FooterImage = () => {
-  const { theme } = useTheme();
+  const { theme, systemTheme } = useTheme();
+
   const { push } = useRouter();
   const handleClick = () => {
     push("/");
   };
   return (
     <>
+      {theme == "light" && (
+        <img
+          src="/logos/steiner-1.png"
+          width={80}
+          height={80}
+          alt="Diluc logo"
+          onClick={handleClick}
+          className="size-20  ml-1 cursor-pointer"
+        />
+      )}
+
       {theme == "dark" && (
         <img
           src="/logos/steiner-2.png"
@@ -21,7 +33,7 @@ const FooterImage = () => {
           className="size-20  ml-1 cursor-pointer"
         />
       )}
-      {theme == "light" && (
+      {theme == "system" && systemTheme == "light" ? (
         <img
           src="/logos/steiner-1.png"
           width={80}
@@ -30,6 +42,27 @@ const FooterImage = () => {
           onClick={handleClick}
           className="size-20  ml-1 cursor-pointer"
         />
+      ) : theme == "system" && systemTheme == "dark" ? (
+        <img
+          src="/logos/steiner-2.png"
+          width={80}
+          height={80}
+          alt="Diluc logo"
+          onClick={handleClick}
+          className="size-20  ml-1 cursor-pointer"
+        />
+      ) : (
+        theme == "system" &&
+        systemTheme == undefined && (
+          <img
+            src="/logos/steiner-1.png"
+            width={80}
+            height={80}
+            alt="Diluc logo"
+            onClick={handleClick}
+            className="size-20  ml-1 cursor-pointer"
+          />
+        )
       )}
     </>
   );
